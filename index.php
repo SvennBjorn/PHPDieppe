85ea98169236deb759eac88f45c575440fb9bdae
Stevan Guillermet <sbg@hotmail.fr>
2018-04-04 14:58:39 +0200

    GET

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 include "./includes/header.php";
+if (isset($_GET['page']) && $_GET['page'] !=""){
+$page = $_GET['page'];
+                                                       
+}
+else {
+    $page = "home";
+}
+
+echo $page;
 
 include "./includes/footer.php";
