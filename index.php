e444a6140658ba1a543fa2a33d56ca0bb0c26e38
Stevan Guillermet <sbg@hotmail.fr>
2018-04-04 16:17:01 +0200

    callPage et Style

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
+<head>
+<link rel="stylesheet" type="text.css" href="./asset/css/style.css">
+</head>
 <?php
 include "./includes/header.php";
-if (isset($_GET['page']) && $_GET['page'] !=""){
-$page = $_GET['page'];
-                                                       
-}
-else {
-    $page = "home";
-}
 
-echo $page;
 
-include "./includes/footer.php";
