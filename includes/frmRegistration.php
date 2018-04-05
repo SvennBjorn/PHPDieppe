9fe0a0efb186e6e091dc44e4a77d41330904960e
Stevan Guillermet <sbg@hotmail.fr>
2018-04-05 09:28:33 +0200

    Formulaire

---
+++ includes/frmRegistration.php
@@ -0,0 +1,22 @@
+<form method="post" action="#">
+    <div>
+        <label for="nom">Nom&nbsp;:</label>
+        <input type="text" name="nom"/>
+    </div>
+    <div>
+        <label for="prenom">Prénom&nbsp;:</label>
+        <input type="text" name="prenom"/>
+    </div>
+    <div>
+        <label for="mail">Mail&nbsp;:</label>
+        <input type="text" name="mail"/>
+    </div>
+    <div>
+        <label for="mdp">Mot de passe&nbsp;:</label>
+        <input type="password" name="mdp"/>
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+    </div>
+</form>
