9f0eaa9d2d1edbc0cdc74d6dc978f97c1d7dfcf6
Stevan Guillermet <sbg@hotmail.fr>
2018-04-06 14:59:16 +0200

    ajout recup

--- includes/mailValidation.inc.php
+++ includes/mailValidation.inc.php
@@ -1,2 +1,38 @@
 <?php
 echo "Ca marche!";
+if (isset($_GET['id']) && isset($_GET['token'])){
+    $id = $_GET['id'];
+    $token = $_GET['token'];
+    $connection = mysqli_connect("localhost","root","","phpdieppe");
+    
+    if (!$connection) {
+        die("Erreur MySQL" . mysqli_connect_errno() ." | " .mysqli_connect_error());
+    }
+    else {
+        $requeteVerif= "SELECT * FROM T_USERS
+                        WHERE ID_USER=$id
+                        AND USETOKEN='$token'";
+        if ($resultatRequete = mysqli_querry($connection,$requeteVerif)){
+            $nbrResultats = mysql_num_rows($resultatRequete);
+            mysqli_free_result($resultatRequete);
+            
+            if ($nbrResultats > 0){
+                $resultatUpdate = "UPDATE T_USERS
+                                    SET USEVERIF=1
+                                    WHERE ID_USER=$id";
+            if (mysqli_query($connection, $requeteUpdate)) {
+                echo "Inscription validée";
+            }
+            else {
+                echo "Inscription pas validée, mais alors pas validée du tout";
+            }
+        }
+        else {
+            echo "<h1>Bien tenté, mais essaie encore</h1>";
+        }
+    }
+    else{
+    echo 'erreur';
+
+
+}
