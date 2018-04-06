47ad9aaa874ac209244b6d74e4afc8005973102e
Stevan Guillermet <sbg@hotmail.fr>
2018-04-06 16:15:30 +0200

    test login

--- includes/frmLogin.php
+++ includes/frmLogin.php
@@ -11,5 +11,5 @@
         <input type="reset" value="Effacer" />
         <input type="submit" value="Envoyer" />
     </div>
-        <input type="hidden" name="frmLogin" />
+    <input type="hidden" name="frmLogin" />
 </form>
--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
             <li><a href="index.php?page=contact">Contact</a></li>
             <li><a href="index.php?page=news">News</a></li>
         </ul>
-
+        
     </nav>
+   
 </header>
